3b08fb05f214bde3b5e2e96fe50896fc7a486751
David Hollinger III <david.hollinger@moduletux.com>
2020-11-30 01:03:01 -0600

    Update xwa.bat

--- installers/xwa/xwa.bat
+++ installers/xwa/xwa.bat
@@ -1,4 +1,18 @@
 @echo off
-FOR /F "tokens=*" %%g IN ('RadioButtonBox.exe "Launch;Configure" "Select to Launch" "X-Wing Alliance Launcher"') do (SET VAR=%%g)
+FOR /F "tokens=*" %%g IN ('RadioButtonBox.exe "Launch;Configure" "Select to Launch" "X-Wing Alliance Launcher"') do (SET launch=%%g)
 
-if %VAR%==Configure (TotalConverter\BabuFriksConfigurator.exe) else (XWINGALLIANCE.EXE)
+IF /i "%launch%"=="Configure" goto :StartConfig
+IF /i "%launch%"=="Launch" goto :LaunchGame
+
+goto commonexit
+
+:StartConfig
+TotalConverter\BabuFriksConfigurator.exe
+goto commonexit
+
+:LaunchGame
+XWINGALLIANCE.EXE
+goto commonexit
+
+:commonexit
+EXIT /B 0
