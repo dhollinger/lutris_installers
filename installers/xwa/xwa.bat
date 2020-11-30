7bf94b7a10ea7c1d37ffdfc8471d9dbe3411aa19
David Hollinger III <david.hollinger@moduletux.com>
2020-11-30 13:18:12 -0600

    Fixed bug in Batch script

--- installers/xwa/xwa.bat
+++ installers/xwa/xwa.bat
@@ -1,5 +1,6 @@
 @echo off
-FOR /F "tokens=*" %%g IN ('RadioButtonBox.exe "Launch;Configure" "Select to Launch" "X-Wing Alliance Launcher"') do (SET launch=%%g)
+
+FOR /F "tokens=*" %%g IN ('START /B RadioButtonBox.exe "Launch;Configure" "Select to Launch" "X-Wing Alliance Launcher"') DO SET launch=%%g
 
 IF /i "%launch%"=="Configure" goto :StartConfig
 IF /i "%launch%"=="Launch" goto :LaunchGame
