ff21ab3b86792ee563d5ad9bcd3e17c00ba045d0
David Hollinger III <david.hollinger@moduletux.com>
2020-12-30 13:09:15 -0600

    Update XWAUpgrade to latest and add TC app to launcher

--- installers/xwa/xwa.bat
+++ installers/xwa/xwa.bat
@@ -1,14 +1,19 @@
 @echo off
 
-for /f %%g IN ('RadioButtonBox.exe "Launch;Configure" "Select to Launch" "X-Wing Alliance Launcher"') do set launch=%%g
+for /f %%g IN ('RadioButtonBox.exe "X-Wing Alliance;Babu Frik's Configurator;Palpatine Total Converter" "Select to Launch" "X-Wing Alliance Launcher"') do set launch=%%g
 
-if /i "%launch%"=="Configure" goto :StartConfig
-if /i "%launch%"=="Launch" goto :LaunchGame
+if /i "%launch%"=="Babu" goto :StartConfig
+if /i "%launch%"=="Palpatine" goto :StartConverter
+if /i "%launch%"=="X-Wing" goto :LaunchGame
 
 goto commonexit
 
 :StartConfig
 TotalConverter\BabuFriksConfigurator.exe
+goto commonexit
+
+:StartConverter
+TotalConverter\PalpatineTotalConverter.exe
 goto commonexit
 
 :LaunchGame
